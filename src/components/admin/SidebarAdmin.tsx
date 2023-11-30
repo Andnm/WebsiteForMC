3819fc5905bbafd6e85ea1780887659eef86dcf6
andnm1508 <dangnguyenminhan123@gmail.com>
2023-11-30 20:44:26 +0700

    fix sidebar

--- src/components/admin/SidebarAdmin.tsx
+++ src/components/admin/SidebarAdmin.tsx
@@ -26,77 +26,106 @@ interface NavLinkProps {
   suffix?: React.ReactNode;
 }
 
-const NavLink: React.FC<NavLinkProps> = ({ href, icon, label, suffix }) => (
-  <Link href={href} className="flex items-center">
-    {icon}
-    <p>{label}</p>
-    {suffix && <ListItemSuffix>{suffix}</ListItemSuffix>}
-  </Link>
-);
+const sections = [
+  {
+    title: "Công cụ",
+    features: [
+      {
+        href: "/dashboard",
+        icon: <SiSimpleanalytics className="w-4 h-4" />,
+        label: "Thống kê",
+      },
+      {
+        href: "/notification",
+        icon: <IoNotificationsOutline className="w-4 h-4" />,
+        label: "Thông báo",
+        suffix: (
+          <Chip
+            value="14"
+            size="sm"
+            variant="ghost"
+            color="indigo"
+            className="rounded-full"
+          />
+        ),
+      },
+    ],
+  },
+  {
+    title: "Quản lý",
+    features: [
+      {
+        href: "/manage-account",
+        icon: <MdOutlineAccountCircle className="w-4 h-4" />,
+        label: "Tài khoản",
+      },
+      { href: "/manage-group", icon: <GrGroup />, label: "Nhóm" },
+      {
+        href: "/manage-project",
+        icon: <GoProjectRoadmap className="w-4 h-4" />,
+        label: "Dự án",
+      },
+      {
+        href: "/manage-report",
+        icon: <GoReport className="w-4 h-4" />,
+        label: "Báo cáo",
+      },
+    ],
+  },
+];
+
+const NavLink: React.FC<NavLinkProps> = ({ href, icon, label, suffix }) => {
+  const pathName = usePathname();
+
+  const activeNavLink = pathName === href;
+
+  return (
+    <Link
+      href={href}
+      className={`flex items-center px-4 py-2 gap-3 rounded-lg ${
+        activeNavLink ? "bg-blue-100" : "hover:bg-gray-200"
+      }`}
+    >
+      {icon}
+      <p>{label}</p>
+      {suffix && <ListItemSuffix>{suffix}</ListItemSuffix>}
+    </Link>
+  );
+};
 
 const SidebarAdmin = () => {
   return (
-    <div className="sidebar-admin h-screen w-full max-w-[15rem] shadow-xl shadow-blue-gray-900/5">
+    <div className="sidebar-admin h-screen w-full max-w-[15rem] shadow-xl shadow-blue-gray-900/5 relative">
       <div className="mb-2 p-4">
         <Typography variant="h5" color="orange" className="text-center">
           MC
         </Typography>
       </div>
 
-      <>
-        <div color="blue-gray" className="flex items-center font-bold">
-          <div className="bg-blue-gray-300 h-0.5 w-5 mr-2"></div>
-          <p className="uppercase text-sm">Công cụ</p>
+      {sections.map((section, index) => (
+        <div key={index}>
+          <div color="blue-gray" className="flex items-center font-bold">
+            <div className="bg-red-300 h-0.5 w-5 mr-2"></div>
+            <p className="uppercase text-sm text-red-300">{section.title}</p>
+          </div>
+
+          <List className="px-4 py-4">
+            {section.features.map((feature, featureIndex) => (
+              <NavLink key={featureIndex} {...feature} />
+            ))}
+          </List>
         </div>
+      ))}
 
-        <List className="px-4 py-4">
-          <NavLink
-            href={"/dashboard"}
-            icon={<SiSimpleanalytics />}
-            label="Thống kê"
-          />
-          <NavLink
-            href={"/notification"}
-            icon={<IoNotificationsOutline />}
-            label="Thông báo"
-            suffix={
-              <Chip
-                value="14"
-                size="sm"
-                variant="ghost"
-                color="blue-gray"
-                className="rounded-full"
-              />
-            }
-          />
-        </List>
-      </>
-
-      <>
-        <div color="blue-gray" className="flex items-center font-bold">
-          <div className="bg-blue-gray-300 h-0.5 w-5 mr-2"></div>
-          <p className="uppercase text-sm">Quản lý</p>
-        </div>
-
-        <List className="px-4 py-4">
-          <NavLink
-            href={"/manage-account"}
-            icon={<MdOutlineAccountCircle />}
-            label="Tài khoản"
-          />
-          <NavLink href={"/manage-group"} icon={<GrGroup />} label="Nhóm" />
-          <NavLink
-            href={"/manage-project"}
-            icon={<GoProjectRoadmap />}
-            label="Dự án"
-          />
-          <NavLink
-            href={"/manage-report"}
-            icon={<GoReport />}
-            label="Báo cáo"
-          />
-        </List>
-      </>
+      <List className="px-4 py-4 absolute bottom-0">
+        <Link
+          href="#"
+          className={`flex items-center px-4 py-2 gap-3 rounded-lg hover:bg-gray-200`}
+        >
+          <RiLogoutBoxRLine />
+          <p>Đăng xuất</p>
+        </Link>
+      </List>
     </div>
   );
 };
