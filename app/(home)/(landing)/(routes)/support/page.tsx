7bf1aa52a38ad356c6616eb29fdeb0d48288e240
andnm1508 <dangnguyenminhan123@gmail.com>
2023-12-08 18:49:31 +0700

    fix support page

--- app/(home)/(landing)/(routes)/support/page.tsx
+++ app/(home)/(landing)/(routes)/support/page.tsx
@@ -13,7 +13,7 @@ const Support = () => {
           </h3>
           <p className="mt-4 leading-7 text-gray-200">
             Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
+            industry. Lorem Ipsum has been the industrys standard dummy text
             ever since the 1500s.
           </p>
 
