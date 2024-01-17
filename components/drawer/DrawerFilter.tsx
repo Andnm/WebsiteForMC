6748a78340c9d614a8c428bab6bc13a328c39752
NganNguyen752001 <nnbngan7501@gmail.com>
2024-01-17 22:05:18 +0700

    add filter

--- components/drawer/DrawerFilter.tsx
+++ components/drawer/DrawerFilter.tsx
@@ -1,5 +1,14 @@
+"use client";
 import React from "react";
 import { Drawer, IconButton } from "@material-tailwind/react";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
 
 interface DrawerFilterProps {
   openDrawer: any;
@@ -13,11 +22,11 @@ const DrawerFilter = ({ openDrawer, closeDrawerAction }: DrawerFilterProps) => {
       placement="right"
       open={openDrawer}
       onClose={closeDrawerAction}
-      className="p-4 shadow-md"
+      className="p-4 shadow-md w-[500px]"
       size={700}
     >
       <div className="mb-6 flex items-center justify-between ">
-        <h5 className="text-black font-bold text-lg">Bộ lọc</h5>
+        <h1 className="text-black font-bold text-2xl">Bộ lọc</h1>
         <IconButton variant="text" onClick={closeDrawerAction}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -34,6 +43,83 @@ const DrawerFilter = ({ openDrawer, closeDrawerAction }: DrawerFilterProps) => {
             />
           </svg>
         </IconButton>
+      </div>
+
+      <div>
+        {/*  Lĩnh vực chuyên môn, specialization_field, Nông nghiệp, Thủ công nghiệp*/}
+        <Accordion type="multiple">
+          <AccordionItem value={`item-1`} className="border-none">
+            <AccordionTrigger
+              className="flex items-center gap-x-2 p-1.5 text-neutral-700 rounded-md hover:bg-neutral-500/10 
+          transition text-start no-underline hover:no-underline"
+              style={{ borderRadius: "6px" }}
+            >
+              <div className="flex items-center gap-x-2">
+                {/* specialization_field */}
+                <span className="font-medium text-sm">Lĩnh vực chuyên môn</span>
+              </div>
+            </AccordionTrigger>
+            <AccordionContent className="pt-1 text-neutral-700">
+              <Button
+                className={cn(
+                  "w-full font-normal justify-start pl-10 mb-1 hover:bg-neutral-500/10 gap-2 rounded-md"
+                )}
+                variant="ghost"
+              >
+                Nông Nghiệp
+              </Button>
+
+              <Button
+                className={cn(
+                  "w-full font-normal justify-start pl-10 mb-1 hover:bg-neutral-500/10 gap-2 rounded-md"
+                )}
+                variant="ghost"
+              >
+                Nông Nghiệp
+              </Button>
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+
+ {/*  Mô hình kinh doanh, business_model: b2b b2c*/}
+        <Accordion type="multiple">
+          <AccordionItem value={`item-2`} className="border-none">
+            <AccordionTrigger
+              className="flex items-center gap-x-2 p-1.5 text-neutral-700 rounded-md hover:bg-neutral-500/10 
+          transition text-start no-underline hover:no-underline"
+              style={{ borderRadius: "6px" }}
+            >
+              <div className="flex items-center gap-x-2">
+                {/* business_model, b2c b2b*/}
+                <span className="font-medium text-sm">Mô hình kinh doanh</span>
+              </div>
+            </AccordionTrigger>
+            <AccordionContent className="pt-1 text-neutral-700">
+              <Button
+                className={cn(
+                  "w-full font-normal justify-start pl-10 mb-1 hover:bg-neutral-500/10 gap-2 rounded-md"
+                )}
+                variant="ghost"
+              >
+                Nông Nghiệp
+              </Button>
+
+              <Button
+                className={cn(
+                  "w-full font-normal justify-start pl-10 mb-1 hover:bg-neutral-500/10 gap-2 rounded-md"
+                )}
+                variant="ghost"
+              >
+                Nông Nghiệp
+              </Button>
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+
+        
+        {/* Hướng đi của dự án, business_type Plan (Lên kế hoạch), Project (Triển khai dự án)*/}
+
+
       </div>
     </Drawer>
   );
